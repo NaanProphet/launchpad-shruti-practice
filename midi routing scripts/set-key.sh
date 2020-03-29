40ef518bbf02567c6d6793af68395d204083dc74
Krishna Bhamidipati <NaanProphet@users.noreply.github.com>
2020-03-28 20:47:41 -0400

    fixing bug in shruti colors
    - NUM_NOTES_OCTAVE + 1 changed to NUM_NOTES_OCTAVE
    - added documentation
    - added better debugging

--- midi routing scripts/set-key.sh
+++ midi routing scripts/set-key.sh
@@ -13,30 +13,30 @@ TRANSPOSE=2
 
 
 COLOR_OUTPUT="lpp-colors.smidi"
-SYSEX_HEADER='0 32 41 2 16 15 1 '
+SYSEX_HEADER='0 32 41 2 16 15 1'
 NUM_NOTES_OCTAVE=22
-COLORS=('25 25 25'
-'30 2 2'
-'0 0 0'
-'0 0 0'
-'0 10 0'
-'5 5 63'
-'0 0 0'
-'0 0 0'
-'0 80 80'
-'24 8 0'
-'0 0 0'
-'0 0 0'
-'15 2 0'
-'33 20 0'
-'70 0 40'
-'0 0 0'
-'0 0 0'
-'18 9 20'
-'30 10 10'
-'0 0 0'
-'0 0 0'
-'80 0 10')
+COLORS=('25  25  25'
+'30  2   2'
+'0   0   0'
+'0   0   0'
+'0   10  0'
+'5   5   63'
+'0   0   0'
+'0   0   0'
+'0   80  80'
+'24  8   0'
+'0   0   0'
+'0   0   0'
+'15  2   0'
+'33  20  0'
+'70  0   40'
+'0   0   0'
+'0   0   0'
+'18  9   20'
+'30  10  10'
+'0   0   0'
+'0   0   0'
+'80  0   10')
 
 
 
@@ -54,9 +54,15 @@ offset=0
 colors=''
 
 
+# read midimap template file (defined at bottom)
 while read -r line; do
+  # identify lines that have the magic placeholder <NUMBER>
   if echo "$line" | grep -qE "${REPLACE_START_CHAR}[0-9]+${REPLACE_END_CHAR}"; then
 
+    # key is from 1 to 64 - number of total keys on the launchpad
+    # row is from from 1 to 8 - refers to the row on the launchpad
+    # offset is for overlapping the same notes across row boundaries 
+    # (launchpad firmware defaults this to min 1, but here it can be zero!)
     key=$(( key+1 ))
     if [ $(( key % 8 )) = 1 ]; then
       row=$(( row+1 ))
@@ -66,9 +72,12 @@ while read -r line; do
     fi
 
 
-    # echo "$key - $row"
-    shruti_index=$(( (key -1 - offset) % (NUM_NOTES_OCTAVE + 1) ))
+	# shruti_index if from 0 to 21, because it's a bash array
+    shruti_index=$(( (key -1 - offset) % (NUM_NOTES_OCTAVE) ))
     color="${COLORS[$shruti_index]}"
+    # since this script doesn't yet have tests, echo for poor man's verification
+    # printf for left padding
+    echo "Key Num `printf %02d $key` - Row $row - Shruti Index `printf %02d $shruti_index` - RGB Color: $color"
     echo "$color" >> "${COLOR_OUTPUT}"
 
     note=$(( (MIDI_C1_NOTE + TRANSPOSE) + (key - 1) - offset ))
